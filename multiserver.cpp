a2d0cfb81485c00b2de7f71196145fabb0e298e7
Rohankumar96506 <59435105+Rohankumar96506@users.noreply.github.com>
2022-01-08 12:28:51 +0530

    Update multiserver.cpp

--- multiserver.cpp
+++ multiserver.cpp
@@ -8,8 +8,10 @@
 #include<unistd.h>
 #include<pthread.h>
 #define CLIENT_SIZE 50
-#define BUFFER_SIZE 100
+#define BUFFER_SIZE 150
 #define NAME_BUFFER_SIZE 25
+#define NAME_BUFFER 180
+#define HOST_ADDR 9001
 
 
 
@@ -42,46 +44,47 @@ void add_in_arr(struct client_data* client_sock)
         }
 }
 
-
+//to concat name and his or her message
+void concat(char name[],char message[],char buffer[])
+{ 
+    strcpy(buffer,name);
+    strcat(buffer,": \0");
+    strcat(buffer,message);
+  
+}
 
 //for handling send and recieve message from client
 void* client_handler(void* args)
 { struct client_data* client = (struct client_data*)args;
   char buffer[BUFFER_SIZE];
-  char message[BUFFER_SIZE + NAME_BUFFER_SIZE];
+  char name_buffer[NAME_BUFFER_SIZE];
   
   while(1)
   {
   memset(buffer,0,sizeof buffer);
-  memset(message,0,sizeof message);
-
-  int flag = recv(client->sock,buffer,sizeof buffer,0);
   
-  if(flag == -1)
-  {
-      printf("ERROR RECIVE");
-      exit(0);
-  }
-  else
+  
+  int flag = recv(client->sock,buffer,sizeof buffer,0);
+  concat(client->name,buffer,name_buffer);
   if(flag>0)
   {   
-      sprintf(message,"%s %s\n",client->name,buffer);
-      printf("%s(%d) has send a message\n",client->name,client->uid);
+      printf("%s(%d) has send a message and packet size is %d\n",client->name,client->uid,flag);
+      
       for(int i = 0;i<CLIENT_SIZE;i++)
       {
           if(client_info[i]!=NULL && client_info[i]->uid!=client->uid)
-          {
-              send(client_info[i]->sock,message,strlen(message),0);
+          {   
+              send(client_info[i]->sock,name_buffer,strlen(name_buffer)+1,0);
           }
       }
 
   }
   else
-  if(flag == 0)
+  if(flag <= 0)
   {
       for(int i = 0;i<CLIENT_SIZE;i++)
       {
-          if(client_info[i]->uid==client->uid)
+          if(client_info[i]!=NULL && client_info[i]->uid==client->uid)
             {
 
             printf("client has left id is %d and name is %s\n",client->uid,client->name);
@@ -93,6 +96,7 @@ void* client_handler(void* args)
             }
 
       }
+      return NULL;
       
 
    
@@ -114,9 +118,8 @@ int main()
     struct sockaddr_storage client_addr;
     struct client_data *pclient;
     no_of_client = 0;
-    user_id = 0;
-    int user_id;                        
-    int name_buffer[NAME_BUFFER_SIZE];  //for storing name get from client
+    int user_id = 0;                        
+    char name_buffer[NAME_BUFFER_SIZE];  //for storing name get from client
 
     memset(&server_addr,0,sizeof server_addr);
 
@@ -129,33 +132,39 @@ int main()
   
     //giving server info
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(4515);
+    server_addr.sin_port = htons(HOST_ADDR);
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     server_sock = socket(PF_INET,SOCK_STREAM,0);
 
 
     if(server_sock==-1)
     {
-        printf("ERROR IN SOCKET");
+        printf("ERROR IN SOCKET\n");
         return 1;
     }
 
     if(bind(server_sock,(struct sockaddr*)&server_addr,sizeof server_addr)==-1)
     {
-        printf("ERROR IN BIND");
+        printf("ERROR IN BIND\n");
         return 1;
     }
 
     if(listen(server_sock,10)==-1)
     {
-        printf("ERROR IN LISTEN");
+        printf("ERROR IN LISTEN\n");
         return 1;
     }
 
     while(1)
     {   
+        
         client_sock = accept(server_sock,(struct sockaddr*)&client_addr,&client_addr_size);
         
+        if(client_sock == -1)
+        {
+            printf("Error in accept\n");
+            return 0;
+        }
 
         if(no_of_client <= CLIENT_SIZE)
         {
@@ -165,14 +174,16 @@ int main()
         int errno = recv(client_sock,name_buffer,sizeof name_buffer,0);
         if(errno == 0)
         {
-            printf("CLIENT HAS LEFT %s\n",buffer);
+            printf("CLIENT HAS LEFT %s\n",name_buffer);
         }
         else
         if(errno < 0)
         {
             printf("Error in RECEIVE\n");
-            return 1;
+            free(pclient);
+            continue;
         }
+        printf("Client has enter the chat %s\n",name_buffer);
         strcpy(pclient->name,name_buffer);
         add_in_arr(pclient);
         no_of_client++;
